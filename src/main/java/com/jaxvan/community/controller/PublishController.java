17d2e771e72594b6ed4a3dfa2ff261281fe59ec0
jaxvan <928620613@qq.com>
2021-11-27 16:38:12 +0800

    add tag

--- src/main/java/com/jaxvan/community/controller/PublishController.java
+++ src/main/java/com/jaxvan/community/controller/PublishController.java
@@ -1,10 +1,11 @@
 package com.jaxvan.community.controller;
 
+import com.jaxvan.community.cache.TagCache;
 import com.jaxvan.community.dto.QuestionDTO;
-import com.jaxvan.community.mapper.UserMapper;
 import com.jaxvan.community.model.Question;
 import com.jaxvan.community.model.User;
 import com.jaxvan.community.service.QuestionService;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -29,11 +30,13 @@ public class PublishController {
         model.addAttribute("description", question.getDescription());
         model.addAttribute("tag", question.getTag());
         model.addAttribute("id", question.getId());
+        model.addAttribute("tags", TagCache.get());
         return "publish";
     }
 
     @GetMapping("/publish")
-    public String publish() {
+    public String publish(Model model) {
+        model.addAttribute("tags", TagCache.get());
         return "publish";
     }
 
@@ -48,17 +51,24 @@ public class PublishController {
         model.addAttribute("title", title);
         model.addAttribute("description", description);
         model.addAttribute("tag", tag);
-        model.addAttribute("id", id);
+        model.addAttribute("tags", TagCache.get());
 
-        // 验证
+        // 验证输入是否为空或非法
         if (title == null || title.length() == 0) {
             model.addAttribute("error", "标题不能为空");
             return "publish";
         }
-        if (description == null || description.length() == 0) {
+        if (StringUtils.isBlank(description)) {
             model.addAttribute("error", "描述内容不能为空");
             return "publish";
         }
+        String invalid = TagCache.filterInvalid(tag);
+        if (StringUtils.isNotBlank(invalid)) {
+            model.addAttribute("error", "标签非法：" + invalid);
+            return "publish";
+        }
+
+        // 验证用户登录
         User user = (User) request.getSession().getAttribute("user");
         if (user == null)  {
             model.addAttribute("error", "用户未登录");
